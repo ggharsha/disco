8b5b1a4f761f8c3574357a9f8b14f5b2eed35634
harsha <ggharshabandi@gmail.com>
2022-01-21 16:55:39 -0600

    Finish controllers, work on jbuilder

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,8 +3,17 @@ class Api::SessionsController < ApplicationController
     before_action :require_logged_out, only: [:create]
 
     def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user
+            login!(@user)
+            render :show
+        else
+            render json: ['Invalid credentials.'], status: 422
+        end
     end
 
     def destroy
+        logout!
+        redirect_to api_session_url
     end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,23 +3,43 @@ class Api::UsersController < ApplicationController
     before_action :require_logged_out, only: [:create]
 
     def index
+        @users = User.all 
+        render :index
     end
 
     def show
+        @user = User.find(params[:id])
+        render :show
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
     end
 
     def update
     end
 
     def destroy
+        @user = User.find(params[:id])
+        if !@user.is_password?(user_params[:password])
+            render json: ['Incorrect password.'], status: 422
+        elsif @user.id = current_user.id
+            @user.destroy
+            render :show
+        else
+            render json: ['You cannot destroy someone else\'s account.'], status: 422
+        end
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :password, :username)
     end
 end
