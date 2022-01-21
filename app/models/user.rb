2d28c83b3ed98e2805c9700d2dcb721c4d864763
harsha <ggharshabandi@gmail.com>
2022-01-21 14:15:43 -0600

    Working on routing and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,28 @@ class User < ApplicationRecord
     after_initialize :ensure_session_token
     attr_reader :password
 
-    
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        return nil unless user
+        user.is_password?(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
 end
