560da23b762f566be1de09df8372551f093e9734
harsha <ggharshabandi@gmail.com>
2022-01-21 15:42:37 -0600

    Configured frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace 
+  namespace :api, defaults: { format: :json } do
+    resources :user, only: [:create, :destroy]
+  end
 
   root to: "static_pages#root"
 end
