0ae0cd010980be255645b7eb894668e0213d703c
harsha <ggharshabandi@gmail.com>
2022-01-21 19:52:40 -0600

    Start components

--- frontend/disco.jsx
+++ frontend/disco.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root'
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     const store = configureStore();
     //
-    ReactDOM.render(<h1>hello from react</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 })
