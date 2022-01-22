13a154d0e7943631022fd538cac189d797863a68
harsha <ggharshabandi@gmail.com>
2022-01-21 19:05:57 -0600

    Added redux for users

--- frontend/disco.jsx
+++ frontend/disco.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    //
-    //
+    const store = configureStore();
     //
     ReactDOM.render(<h1>hello from react</h1>, root);
 })
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,8 @@
+import entitiesReducer from "./entities_reducer";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    entities: entitiesReducer
+});
+
+export default rootReducer;
