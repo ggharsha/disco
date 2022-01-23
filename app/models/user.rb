40638498bbabc905272e4658cf02ab30eafee7d9
harsha <ggharshabandi@gmail.com>
2022-01-23 15:58:06 -0600

    Edit user model to handle user handle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-    validates :username, :tag, :email, :password_digest, :session_token, presence: true
+    validates :username, :tag, :email, :password_digest, :session_token, :handle, presence: true
     validates :email, :session_token, uniqueness: true
     validates :username, uniqueness: { scope: :tag }
     validates :password, length: { minimum: 8 }, allow_nil: true
-    after_initialize :ensure_session_token
+
+    after_initialize :ensure_session_token, :assign_tag
+
     attr_reader :password
 
     def self.find_by_credentials(email, password)
@@ -29,5 +31,23 @@ class User < ApplicationRecord
 
     def ensure_session_token
         self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    def assign_tag
+        self.tag ||= build_tag
+        @handle = username + '#' + tag
+    end
+
+    def build_tag
+        num = rand(1..9999)
+        if num < 10
+            num = "000" + num.to_s
+        elsif num < 100
+            num = "00" + num.to_s
+        elsif num < 1000
+            num = "0" + num.to_s
+        else 
+            num = num.to_s
+        end
     end
 end
--- frontend/disco.jsx
+++ frontend/disco.jsx
@@ -3,22 +3,33 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root'
 
+//testing these
+import { createUser } from './actions/users_actions';
+import { login } from './actions/session_actions';
+//done testing
+
 document.addEventListener('DOMContentLoaded', () => {
     let store;
     if (window.currentUser) {
         const preloadedState = {
             session: { id: window.currentUser.id },
-            entities: {
-                users: { [window.currentUser.id]: window.currentUser }
+            entities: { 
+                users: { [window.currentUser.id]: window.currentUser } 
             }
         };
         store = configureStore(preloadedState);
         delete window.currentUser;
     } else {
         store = configureStore();
-    }
+    };
+
+
+    // testing these
     window.getState = store.getState;
-    //
+    window.createUser = createUser;
+    window.login = login;
+    // done testing
+
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root);
-})
+});
