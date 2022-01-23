a0fd0bdb2b5734f95cd95002142a1369d11fb1d1
harsha <ggharshabandi@gmail.com>
2022-01-23 15:59:31 -0600

    Fix attr_reader

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
     after_initialize :ensure_session_token, :assign_tag
 
-    attr_reader :password
+    attr_reader :password, :handle
 
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
