22b07a6097607b3d3429837105bc7e56ee7e6dfc
harsha <ggharshabandi@gmail.com>
2022-01-27 22:53:32 -0600

    Dont question it

--- frontend/components/me/me.jsx
+++ frontend/components/me/me.jsx
@@ -7,10 +7,15 @@ export default class Me extends React.Component {
         return (
             <div id="me-container">
                 <div id="dm-container">
+                    <div id="dm-line">
+
+                    </div>
                     {/* direct message container will go here*/}
                 </div>
                 <div id="friend-container">
-                    
+                    <div id="friend-line">
+
+                    </div>
                 </div>
             </div>
         )
