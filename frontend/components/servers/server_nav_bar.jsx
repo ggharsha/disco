e49e9f490aab4f037f6fcfa8e9ab6bf12bbcfb3b
harsha <ggharshabandi@gmail.com>
2022-02-04 12:19:34 -0600

    Added images

--- frontend/components/servers/server_nav_bar.jsx
+++ frontend/components/servers/server_nav_bar.jsx
@@ -24,6 +24,7 @@ export default class ServerNavBar extends React.Component {
             openModal,
             fetchChannel 
         } = this.props;
+
         if ((!this.props.currentUser && servers[0]) || !servers) return null
         return (
             <div>
