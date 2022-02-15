58adf139cd385f06846618eaef54762082efb0df
harsha <ggharshabandi@gmail.com>
2022-02-15 13:43:22 -0600

    Reworking conversation membership backend

--- app/controllers/api/conversation_memberships_controller.rb
+++ app/controllers/api/conversation_memberships_controller.rb
@@ -1,17 +1,17 @@
 class Api::ConversationMembershipsController < ApplicationController
     def create
-        @conversation = Conversation.new(owner_id: current_user.id)
-        conversation_membership = ConversationMembership.new(conversation_id: @conversation.id, member_id: current_user.id)
-        other_conversation_membership = ConversationMembership.new(conversation_id: @conversation.id, member_id: User.find_by(handle: params[:handle]))
-        if conversation_membership.save && other_conversation_membership.save
+        user = User.find_by(handle: params[:handle])
+        @conversation_membership = ConversationMembership.new(member_id: user.id, conversation_id: params[:conversation_id])
+        @conversation = Conversation.find(params[:conversation_id])
+        if @conversation_membership.save
             render 'api/conversations/show'
         else
-            render json: conversation_membership.errors.full_messages, status: 422
+            render json: @conversation_membership.errors.full_messages, status: 422
         end
     end
 
     def destroy
-        @conversation_membership = ConversationMembership.find(params[:id])
+        @conversation_membership = ConversationMembership.find_by(conversation_membership_params)
         if @conversation_membership.member_id == current_user.id && @conversation_membership.destroy
             render 'api/conversations/index'
         else
@@ -21,6 +21,6 @@ class Api::ConversationMembershipsController < ApplicationController
 
     private
     def conversation_membership_params
-        params.require(:conversation_membership).permit(:handle)
+        params.require(:conversation_membership).permit(:handle, :conversation_id)
     end
 end
