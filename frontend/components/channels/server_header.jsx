19e55ed603d4c20f588f85002bdb938782def1bb
harsha <ggharshabandi@gmail.com>
2022-02-22 11:41:22 -0600

    Refactoring server header with hooks

---
+++ frontend/components/channels/server_header.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { useState } from "react";
+
+const ServerHeader = ({ server, openModal, currentUser }) => {
+    const [menu, setMenu] = useState(false);
+
+    const handleClick = () => {
+        if (menu) setMenu(false);
+        else setMenu(true);
+    };
+
+    const ownerOptions = (
+        <div className="server-header-options">
+            <ul>
+                <li 
+                className="server-header-option"
+                onClick={() => openModal("updateServer")}
+                >
+                    Update Server
+                </li>
+                <li 
+                className="server-header-option leave-server"
+                onClick={() => openModal("leaveServer")}
+                >
+                    Leave Server
+                </li>
+            </ul>
+        </div>
+    );
+
+    const memberOptions = (
+        <div className="server-header-options">
+            <ul>
+                <li 
+                className="server-header-option leave-server"
+                onClick={() => openModal("leaveServer")}
+                >
+                    Leave Server
+                </li>
+            </ul>
+        </div>
+    );
+
+    const menuIcon = setMenu ? (
+        <p
+            className="server-name-down-arrow"
+            onClick={() => handleClick()}
+        >
+            &#8964;
+        </p>
+    ) : (
+        <p
+            className="server-name-down-arrow"
+            onClick={() => handleClick()}
+        >
+            &times;
+        </p>
+    )
+
+    return (
+        <div className="server-name">
+            <p className="server-name-text">
+                {server.serverName}
+            </p>
+            {menuIcon}
+        </div>
+    );
+};
+
+export default ServerHeader;
